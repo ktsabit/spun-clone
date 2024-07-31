403dbcbbe0f7e1165b11b3a1ac23c034296ff454
ktsabit <53921453+ktsabit@users.noreply.github.com>
2024-08-01 02:14:57 +0700

    Update Card.tsx

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -20,11 +20,13 @@ export function Card({
   currency,
 }: CardProps) {
 
-  const formatDate = (dateString:string) => {
-    const date = new Date(dateString);
-    const options = { day: '2-digit', month: 'short', year: 'numeric' };
-    return date.toLocaleDateString('en-GB', options);
-  }
+const formatDate = (value) => {
+    let date = new Date(value);
+    const day = date.toLocaleString('default', { day: '2-digit' });
+    const month = date.toLocaleString('default', { month: 'short' });
+    const year = date.toLocaleString('default', { year: 'numeric' });
+    return day + ' ' + month + ' ' + year;
+}
 
   const priceFmt = (number:number) => {
     return new Intl.NumberFormat('id-ID', {
